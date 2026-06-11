973fba1c258ad97dd0384c2a5f3e1a307bb75087
JVarshini1 <varshinijamalapuram490@gmail.com>
2026-06-11 21:45:37 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class String1 {
+public static void main(String[] args) {
+String s="hello world";
+System.out.println(s);
+s=s+" to all ";
+System.out.println(s);
+}
+}
