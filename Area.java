db81e7a4b20fdbdb836120a744d769560ac5ee6f
Varshini <varshinijamalapuram490@gmail.com>
2026-06-16 14:38:39 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,11 @@
+//prog to find area of the triangle
+class Area
+{
+public static void main(String[] args){
+       //ar=05*b*h
+double b=4.5;
+double h=6.7;
+double ar=05*b*h;
+System.out.println("Area: "+ar);
+}
+}
