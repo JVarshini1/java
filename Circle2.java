39d2db793eda04e515bcce8212395f54a04c89a6
Varshini <varshinijamalapuram490@gmail.com>
2026-06-16 15:12:22 +0530

    Add files via upload

---
+++ Circle2.java
@@ -0,0 +1,9 @@
+class Circle2 {
+       static void findArea(double r) {
+        System.out.println("Area:" + Math.PI * r * r);
+    }
+
+    public static void main(String[] args) {
+                findArea(2.5);
+    }
+}
