6d1e0ece2025a1b037d3125bb2933baef2593071
Varshini <varshinijamalapuram490@gmail.com>
2026-06-16 15:11:14 +0530

    Add files via upload

---
+++ Circle1.java
@@ -0,0 +1,13 @@
+class Circle1 
+{
+public static void main(String[] args)
+{
+
+double r=Double.parseDouble(args[0]);
+double ar=Math.PI*r*r;
+double cr=2*Math.PI*r;
+System.out.println("Area of Circle:"+ar);
+System.out.println("Circumference of Circle:"+cr);
+}
+}
+
