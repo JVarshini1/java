f6052cbcd27c46008a831575798b993e9adf3be9
Varshini <varshinijamalapuram490@gmail.com>
2026-06-16 15:06:45 +0530

    Add files via upload

---
+++ Areaf.java
@@ -0,0 +1,11 @@
+//prog to find area of the triangle
+class Areaf
+{
+public static void main(String[] args){
+       //ar=05*b*h
+float b=4.5f;
+float h=6.7f;
+float ar=(Float)(05*b*h);
+System.out.println("Area: "+ar);
+}
+}
