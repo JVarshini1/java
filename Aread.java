215fcd69aa6f39ae4a5fe897f7683eba1dee9785
Varshini <varshinijamalapuram490@gmail.com>
2026-06-16 14:42:34 +0530

    Add files via upload

---
+++ Aread.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Aread
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+double b=sc.nextDouble();
+double h=sc.nextDouble();
+double ar=0.5*b*h;
+System.out.println("Area: "+ar);
+}
+}
